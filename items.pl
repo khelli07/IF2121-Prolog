888550637ff81c252f2e3ecb3886efd5b19d4c6d
Mohamad Daffa Argakoesoemah <argakoesoemahmdaffa@gmail.com>
2021-11-16 23:47:45 +0700

    add tool, seed, fish

---
+++ items.pl
@@ -0,0 +1,60 @@
+/* items.pl */
+
+/* Fakta */
+
+/* TOOLS*/
+/* tool(X,Y): X adalah tool milik Y */
+tool('hoe',farmer).
+tool('fishing rod',fisherman).
+
+/* SEASON */
+/* season(X): X adalah season */
+season(spring).
+season(summer).
+season(fall).
+season(winter).
+
+/* SEEDS */
+/* seed(X): X adalah seed */
+seed('turnip').
+seed('potato').
+seed('cucumber').
+seed('strawberries').
+seed('cabbage').
+seed('moondrop flower').
+seed('toy flower').
+
+seed('tomato').
+seed('corn').
+seed('onion').
+seed('pumpkin').
+seed('pineappler').
+seed('pink cat flower').
+
+seed('eggplant').
+seed('carrot').
+seed('yam').
+seed('spinach').
+seed('green pepper').
+seed('adzuki beans').
+seed('chili peppers').
+seed('blue magic red flower').
+seed('true magic red flower').
+seed('sunsweet flower').
+
+/* FISH */
+/* fish(X): X adalah fish */
+fish('arowana').
+fish('beltfish').
+fish('carp').
+fish('char').
+fish('cherry trout').
+fish('honmoroko').
+fish('keiji salmon').
+fish('killfish').
+fish('lake prawn').
+fish('large crucian carp').
+fish('large sea bass').
+fish('large snakehead').
+
+
