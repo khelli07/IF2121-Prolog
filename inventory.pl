1ba3f0af2b63dc5166289a01444839b56dae722c
Maria Khelli <71018093+khelli07@users.noreply.github.com>
2021-11-21 16:01:33 +0700

    inventory kelar

--- inventory.pl
+++ inventory.pl
@@ -24,14 +24,27 @@ writeInventoryItem([H|T]):-
     writeInventoryItem(T).
 
 isItemIn([], _, 0).
-isItemIn([H|T], Item, Bool):-
-    itemName(Item, Name),
+isItemIn([H|T], Name, Bool):-
     itemName(H, HeadName),
-    (HeadName == Name 
+    toUpper(HeadName, HeadUpper),
+    toUpper(Name, NameUpper),
+
+    (HeadUpper == NameUpper
         ->  Bool1 is 1, !;
-    HeadName \== Name
-        ->  isItemIn(T, Item, Bool1)),
+    HeadUpper \== NameUpper
+        ->  isItemIn(T, Name, Bool1)),
     Bool is Bool1.
+
+getItem([H|T], ItemName, Item):-
+    itemName(H, HeadName),
+    toUpper(HeadName, HeadUpper),
+    toUpper(ItemName, NameUpper),
+
+    (HeadUpper == NameUpper
+        ->  Item1 = H, !;
+    HeadUpper \== NameUpper
+        ->  getItem(T, ItemName, Item1)),
+    Item = Item1.
 
 insertItem([], Item, [Item]).
 insertItem([H|T], Item, NL):-
@@ -49,9 +62,41 @@ insertItem([H|T], Item, NL):-
         ->  insertItem(T, Item, NL1),
             NL = [H|NL1]).
 
-deleteItem([Item], Item, []).
-deleteItem([H|T], Item, []):-
-    write('Hi, this is under construction').
+deleteItem([Item], Item1, NL):-
+    itemName(Item, Name),
+    
+    itemCount(Item, Count),
+    itemCount(Item1, Decrement),
+    NewValue is Count - Decrement,
+
+    (NewValue == 0 
+        -> NL = [], !;
+    NewValue > 0 
+        -> H1 = [Name, NewValue],
+           NL = [H1], !;
+    NewValue < 0
+        -> format('You don\'t have that many ~w. Cancelling..', [Name])).
+
+deleteItem([H|T], Item, NL):-
+    itemName(Item, Name),
+    itemName(H, HeadName),
+    (HeadName == Name 
+        -> itemCount(H, Count),
+           itemCount(Item, Decrement),
+           NewValue is Count - Decrement,
+           (
+               NewValue > 0 
+                    -> H1 = [HeadName, NewValue],
+                       NL = [H1|T], !;
+               NewValue == 0 
+                    -> NL = T, !;
+               NewValue < 0 
+                    -> format('You don\'t have that many ~w. Cancelling..', [HeadName])
+           ), !;
+
+    HeadName \== Name
+        -> deleteItem(T, Item, NL1),
+           NL = [H|NL1]).
 
 /* Bag manipulation */
 saveToBag(Item):-
@@ -66,12 +111,51 @@ saveToBag(Item):-
     Sum + Count > 100 
         ->  write('Your inventory is full!')).
 
-throwItem:-
-    baglist(OldBag).
+throwFromBag(Item):-
+    baglist(OldBag),
+    deleteItem(OldBag, Item, UpdatedBag),
+    asserta(baglist(UpdatedBag)),
+    retract((baglist(OldBag))).
 
-/* Bag output */
+/* Bag command */
 inventory:-
     baglist(List),
     itemSum(List, Sum), nl,
     format('Your inventory capacity (~w/100)', [Sum]), nl,
     writeInventoryItem(List).
+
+throwItem:-
+    baglist(List),
+    write('What do you want to throw? (Use \'\', e.g. \'fishing rod\')'), nl,
+    read(_ItemName), nl,
+    isItemIn(List, _ItemName, Bool),
+    (
+        Bool == 1
+            -> write('How many? '), read_integer(X),
+               Item = [_ItemName, X],
+               throwFromBag(Item), !;
+        Bool == 0
+            -> write('You don\'t even have that item :(')
+    ).
+    
+
+/* utils */
+charUpper(String, Index, CU):-
+    sub_atom(String, Index, 1, _, Char),
+    lower_upper(Char, CU).
+
+stringUpper(String, Index, UpperStr):-
+    atom_length(String, Length),
+    Index == Length,
+    UpperStr = '', !.
+
+stringUpper(String, Index, UpperStr):-
+    atom_length(String, Length),
+    Index < Length,
+    charUpper(String, Index, CU),
+    Index1 is Index + 1,
+    stringUpper(String, Index1, UpperStr1),
+    atom_concat(CU, UpperStr1, UpperStr).
+
+toUpper(String, Output):-
+    stringUpper(String, 0, Output).
